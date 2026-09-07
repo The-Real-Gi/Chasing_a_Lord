136f63b3179d4ac7ca88d20f48dcd7dc91370747
Jan <jan.farkas.sk@gmail.com>
2026-09-07 09:55:31 +0200

    creating another skill 3/5
    creating dropping bombs skill

--- Assets/Scripts/MageBossScripts/ProjectilesScript/DripBomb.cs
+++ Assets/Scripts/MageBossScripts/ProjectilesScript/DripBomb.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class DripBomb : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float fallSpeed = 3f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private bool hasExploded;
+    public int damage;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("IsFlying",true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector2.down * fallSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasExploded)
+        {
+            return;
+        }
+
+        hasExploded = true;
+
+        PlayerScript player = collision.gameObject.GetComponentInParent<PlayerScript>();
+        if (player != null && !player.IsBlocking())
+        {
+            player.TakeDamage(damage, transform);
+        }
+
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+
+        anim.SetBool("IsFlying", false);
+        anim.SetBool("Explosion", true);
     }
 }
