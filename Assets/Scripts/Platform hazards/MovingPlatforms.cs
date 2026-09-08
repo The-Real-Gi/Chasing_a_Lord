21565a567a98f136e06a72b0e27bc61db4d28fff
Jan <jan.farkas.sk@gmail.com>
2026-09-08 21:40:40 +0200

    level design
    making  chain with ball
    making bridge

---
+++ Assets/Scripts/Platform hazards/MovingPlatforms.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float moveDuration = 2f;
+
+    Rigidbody2D rb;
+    float moveTimer;
+    float moveDirection = 1f;
+    readonly HashSet<PlayerScript> playersTouching = new();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        moveTimer += Time.fixedDeltaTime;
+
+        if (moveTimer >= moveDuration)
+        {
+            moveTimer = 0f;
+            moveDirection *= -1f;
+        }
+
+        rb.linearVelocity = new Vector2(moveDirection * moveSpeed, rb.linearVelocity.y);
+
+        foreach (PlayerScript player in playersTouching)
+        {
+            player.rb.position += Vector2.right * rb.linearVelocity.x * Time.fixedDeltaTime;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerScript player = collision.collider.GetComponentInParent<PlayerScript>();
+        if (player != null)
+        {
+            playersTouching.Add(player);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        PlayerScript player = collision.collider.GetComponentInParent<PlayerScript>();
+        if (player != null)
+        {
+            playersTouching.Remove(player);
+        }
+    }
+}
